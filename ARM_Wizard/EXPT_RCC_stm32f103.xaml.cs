2c2f80ac6ab27a584416e0a84ad13fa6887dbb94
mahyar1284 <mahyarshokraeian@live.com>
2019-05-09 15:15:32 +0430

    50% of RCC GUI is completed

--- ARM_Wizard/EXPT_RCC_stm32f103.xaml.cs
+++ ARM_Wizard/EXPT_RCC_stm32f103.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using RCC_Register;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,28 +12,42 @@ namespace ARM_Wizard
     /// </summary>
     public sealed partial class EXPT_RCC_stm32f103 : Page
     {
+        string outstr;
         bool pll_input = false;
         public EXPT_RCC_stm32f103()
         {
             this.InitializeComponent();
+            outstr = "RCC->CR = " + "(" + RCC_CR_Val.HSEON.ToString() + "<<" + RCC_CR.HSEON.ToString() + ") |" +
+                  "(" + RCC_CR_Val.HSITRIM.ToString() + "<<" + RCC_CR.HSITRIM.ToString() + ") |" +
+                  "(" + RCC_CR_Val.HSICAL.ToString() + "<<" + RCC_CR.HSICAL.ToString() + ") |" +
+                  "(" + RCC_CR_Val.HSEON.ToString() + "<<" + RCC_CR.HSEON.ToString() + ") |" +
+                  "(" + RCC_CR_Val.HSEBYP.ToString() + "<<" + RCC_CR.HSEBYP.ToString() + ") |" +
+                  "(" + RCC_CR_Val.CSSON.ToString() + "<<" + RCC_CR.CSSON.ToString() + ") |" +
+                  "(" + RCC_CR_Val.PLLON.ToString() + "<<" + RCC_CR.PLLON.ToString() + ") |";
+
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            RadioButton rb = sender as RadioButton;
-            if (rb != null)
-            {
-                string colorName = rb.Tag.ToString();
-                switch (colorName)
-                {
-                    case "KLS":
-                        pll_input = true;
-                        break;
-                    case "DKLS":
-                        pll_input = false;
-                        break;
-                }
-            }
+            //RadioButton rb = sender as RadioButton;
+            //if (rb != null)
+            //{
+            //    string colorName = rb.Tag.ToString();
+            //    switch (colorName)
+            //    {
+            //        case "KLS":
+            //            pll_input = HSI;
+            //            break;
+            //        case "DKLS":
+            //            pll_input = HSE;
+            //            break;
+            //    }
+            //}
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ ARM_Wizard/Registers_stm32f103.cs
@@ -0,0 +1,29 @@
+﻿namespace RCC_Register
+{
+    public enum RCC_CR : byte
+    {
+        HSION = 0,
+        HSIRDY = 1,
+        HSITRIM = 3,
+        HSICAL = 8,
+        HSEON = 16,
+        HSERDY = 17,
+        HSEBYP = 18,
+        CSSON = 19,
+        PLLON = 24,
+        PLLRDY = 25
+    }
+    public enum RCC_CR_Val : byte
+    {
+        HSION = 0,
+        HSIRDY = 0,
+        HSITRIM = 0,
+        HSICAL = 0,
+        HSEON = 0,
+        HSERDY = 0,
+        HSEBYP = 0,
+        CSSON = 0,
+        PLLON = 0,
+        PLLRDY = 0
+    }
+}
